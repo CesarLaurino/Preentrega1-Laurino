4eb7c392b32f49f984ae29ecaf081e2c3d0a572b
cesar laurino <cesarlaurino@hotmail.com>
2023-02-07 14:41:30 -0300

    borre lo innecesario, agrege las imagenes y cree y adapte los componentes

---
+++ src/components/ItemListContainer.jsx
@@ -0,0 +1,11 @@
+
+const ItemListContainer = (prop) => {
+    return (
+        <div  className="mensaje">
+            <h1 className="prop">{prop.mensaje}</h1>
+        </div>
+
+    )
+}
+
+export default ItemListContainer;
